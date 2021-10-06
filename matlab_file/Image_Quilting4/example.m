5b8eaa8fe74e89561adc3ad2492e4cef1a0ad17d
Albert Simanjuntak <albert.p@bukalapak.com>
2021-10-06 23:07:32 +0700

    update code

--- matlab_file/Image_Quilting4/example.m
+++
@@ -1,9 +0,0 @@
-texture = imread('C:\xampp\htdocs\api\matlab_file\Image_Quilting4\jsquares.gif');
-
-t2 = synthesize(texture, 4 * size(texture), 12, 2);
-
-figure(1)
-imshow(texture);
-figure(2)
-imshow(uint8(t2));
-imwrite(uint8(t2), 'newFileName.png', 'Transparency', [0 0 0]);
